33fb7277dfd06ccdbe1ede05fb5d383013f6d2ce
Petr Broz <ipetrbroz@gmail.com>
2020-05-07 12:21:42 +0200

    Create a bucket if it doesn't exist.

--- routes/docs.js
+++ routes/docs.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const formidable = require('formidable');
-const { DataManagementClient, ModelDerivativeClient, urnify } = require('forge-server-utils');
+const { DataManagementClient, ModelDerivativeClient, DataRetentionPolicy, urnify } = require('forge-server-utils');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, FORGE_BUCKET } = process.env;
 
@@ -10,10 +10,18 @@ let dataManagementClient = new DataManagementClient(auth);
 let modelDerivativeClient = new ModelDerivativeClient(auth);
 let router = express.Router();
 
+async function ensureBucketExists() {
+    const buckets = await dataManagementClient.listBuckets();
+    if (!buckets.find(bucket => bucket.bucketKey === FORGE_BUCKET)) {
+        await dataManagementClient.createBucket(FORGE_BUCKET, DataRetentionPolicy.Temporary);
+    }
+}
+
 // GET /api/docs
 // Lists all designs available in an app-specific bucket.
 router.get('/', async (req, res) => {
     try {
+        await ensureBucketExists();
         const objects = await dataManagementClient.listObjects(FORGE_BUCKET);
         res.json(objects.map(function (obj) {
             return { id: urnify(obj.objectId), name: obj.objectKey };
@@ -36,6 +44,7 @@ router.post('/', (req, res, next) => {
         try {
             const { file } = files;
             const buff = fs.readFileSync(file.path);
+            await ensureBucketExists();
             const object = await dataManagementClient.uploadObject(FORGE_BUCKET, file.name, file.type, buff);
             await modelDerivativeClient.submitJob(urnify(object.objectId), [{ type: 'svf', views: ['2d', '3d'] }], fields['entrypoint-in-zip']);
             res.status(200).end();
