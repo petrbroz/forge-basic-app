bfdd616a82bdfff1b4a6d87bd3e7bd0f2a5fed46
Petr Broz <ipetrbroz@gmail.com>
2020-05-11 16:14:42 +0200

    Switched to the official Forge SDK.

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,27 @@
 const express = require('express');
-const { AuthenticationClient } = require('forge-server-utils');
+const { AuthClientTwoLegged } = require('forge-apis');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
+const publicAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, ['viewables:read'], true);
+let publicTokenPromise = null;
 
-let authClient = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
+async function getPublicToken() {
+    if (!publicTokenPromise) {
+        publicTokenPromise = publicAuthClient.authenticate();
+        publicTokenPromise.then(function (credentials) {
+            setTimeout(function () { publicTokenPromise = null; }, credentials.expires_in * 1000);
+        });
+    }
+    return publicTokenPromise;
+}
+
 let router = express.Router();
 
 // GET /api/auth/token
 // Generates a two-legged token to be used by Forge Viewer.
 router.get('/token', async function (req, res) {
     try {
-        const token = await authClient.authenticate(['viewables:read']);
+        const token = await getPublicToken();
         res.json(token);
     } catch(err) {
         console.error(err);
--- routes/models.js
+++ routes/models.js
@@ -1,20 +1,41 @@
 const fs = require('fs');
 const express = require('express');
 const formidable = require('formidable');
-const { DataManagementClient, ModelDerivativeClient, DataRetentionPolicy, urnify } = require('forge-server-utils');
+const { AuthClientTwoLegged, BucketsApi, ObjectsApi, DerivativesApi } = require('forge-apis');
 
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET } = process.env;
-const BUCKET = `${FORGE_CLIENT_ID}-myfirstapp`;
+const BUCKET = `${FORGE_CLIENT_ID.toLowerCase()}-myfirstapp`;
+const SCOPES = ['bucket:read', 'bucket:create', 'data:read', 'data:create', 'data:write'];
+const privateAuthClient = new AuthClientTwoLegged(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, SCOPES, true);
+let privateTokenPromise = null;
 
-let auth = { client_id: FORGE_CLIENT_ID, client_secret: FORGE_CLIENT_SECRET };
-let dataManagementClient = new DataManagementClient(auth);
-let modelDerivativeClient = new ModelDerivativeClient(auth);
+function urnify(id) {
+    return new Buffer(id).toString('base64');
+}
+
+async function getPrivateToken() {
+    if (!privateTokenPromise) {
+        privateTokenPromise = privateAuthClient.authenticate();
+        privateTokenPromise.then(function (credentials) {
+            setTimeout(function () { privateTokenPromise = null; }, credentials.expires_in * 1000);
+        });
+    }
+    return privateTokenPromise;
+}
+
+let bucketsClient = new BucketsApi();
+let objectsClient = new ObjectsApi();
+let derivativesClient = new DerivativesApi();
 let router = express.Router();
 
-async function ensureBucketExists() {
-    const buckets = await dataManagementClient.listBuckets();
-    if (!buckets.find(bucket => bucket.bucketKey === BUCKET)) {
-        await dataManagementClient.createBucket(BUCKET, DataRetentionPolicy.Temporary);
+async function ensureBucketExists(credentials) {
+    const buckets = await bucketsClient.getBuckets({}, privateAuthClient, credentials);
+    if (!buckets.body.items.find(bucket => bucket.bucketKey === BUCKET)) {
+        const payload = {
+            bucketKey: BUCKET,
+            policyKey: 'transient'
+        };
+        await bucketsClient.createBucket(payload, {}, privateAuthClient, credentials);
     }
 }
 
@@ -22,9 +43,10 @@ async function ensureBucketExists() {
 // Lists all designs available in an app-specific bucket.
 router.get('/', async (req, res) => {
     try {
-        await ensureBucketExists();
-        const objects = await dataManagementClient.listObjects(BUCKET);
-        res.json(objects.map(function (obj) {
+        const credentials = await getPrivateToken();
+        await ensureBucketExists(credentials);
+        const objects = await objectsClient.getObjects(BUCKET, {}, privateAuthClient, credentials);
+        res.json(objects.body.items.map(function (obj) {
             return { id: urnify(obj.objectId), name: obj.objectKey };
         }));
     } catch (err) {
@@ -45,9 +67,25 @@ router.post('/', (req, res, next) => {
         try {
             const { file } = files;
             const buff = fs.readFileSync(file.path);
-            await ensureBucketExists();
-            const object = await dataManagementClient.uploadObject(BUCKET, file.name, file.type, buff);
-            await modelDerivativeClient.submitJob(urnify(object.objectId), [{ type: 'svf', views: ['2d', '3d'] }], fields['entrypoint-in-zip']);
+            const credentials = await getPrivateToken();
+            await ensureBucketExists(credentials);
+            const object = await objectsClient.uploadObject(BUCKET, file.name, file.size, buff, {}, privateAuthClient, credentials);
+            const payload = {
+                input: {
+                    urn: urnify(object.body.objectId)
+                },
+                output: {
+                    formats: [{
+                        type: 'svf',
+                        views: ['2d', '3d']
+                    }]
+                }
+            };
+            if (fields['entrypoint-in-zip']) {
+                payload.input.compressedUrn = true;
+                payload.input.rootFilename = fields['entrypoint-in-zip'];
+            }
+            await derivativesClient.translate(payload, {}, privateAuthClient, credentials);
             res.status(200).end();
         } catch (err) {
             console.error(err);
