1957cc6f663b43f9252fe0c4455ed4b0aa1f5492
Petr Broz <ipetrbroz@gmail.com>
2020-10-30 09:04:38 +0100

    Added basic authentication.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const auth = require('express-basic-auth');
 const { ensureBucketExists } = require('./services/forge');
 
 const port = process.env.PORT || 3000;
 
 let app = express();
 app.use(express.static('public'));
+app.use(auth({
+    users: { 'admin': 'supersecret' },
+    challenge: true
+}));
 app.use('/api/models', require('./routes/models'));
 app.use('/api/auth', require('./routes/auth'));
 ensureBucketExists()
