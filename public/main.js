0ae9153940ea7f5cd3bffcac317368dbcf71e4df
Petr Broz <ipetrbroz@gmail.com>
2021-02-12 09:55:55 +0100

    Removing unnecessary backend code.

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 /// import * as Autodesk from "@types/forge-viewer";
 
-Autodesk.Viewing.Initializer({ getAccessToken }, async function () {
+Autodesk.Viewing.Initializer({ accessToken: '' }, async function () {
     const viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('preview'));
     viewer.start();
     viewer.setTheme('light-theme');
@@ -8,19 +8,3 @@ Autodesk.Viewing.Initializer({ getAccessToken }, async function () {
     //viewer.loadModel('models/rac_basic_sample_project/gltf/model.gltf');
     viewer.loadModel('models/Sponza/glTF/Sponza.gltf');
 });
-
-/**
- * Retrieves access token required for viewing models.
- * @async
- * @param {function} callback Callback function to be called with access token and expiration time (in seconds).
- */
-async function getAccessToken(callback) {
-    const resp = await fetch('/api/auth/token');
-    if (resp.ok) {
-        const { access_token, expires_in } = await resp.json();
-        callback(access_token, expires_in);
-    } else {
-        alert('Could not obtain access token. See the console for more details.');
-        console.error(await resp.text());
-    }
-}
--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-/*
- * Command-line script for running the server application.
- * Usage:
- *   node server.js
- * Environment variables:
- *   FORGE_CLIENT_ID
- *   FORGE_CLIENT_SECRET
- *   FORGE_BUCKET (optional)
- *   PORT (optional)
- */
-
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 let app = express();
 app.use(express.static('public'));
-app.use(require('./routes/api.js'));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
