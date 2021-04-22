21f529ccc4194daa6c7c8fffcaf5514e22d096f8
Petr Broz <ipetrbroz@gmail.com>
2021-04-22 13:17:04 +0200

    Simple demo showing multiple viewers on a single page.

--- public/main.js
+++ public/main.js
@@ -1,12 +1,19 @@
 /// import * as Autodesk from "@types/forge-viewer";
 
 Autodesk.Viewing.Initializer({ getAccessToken }, async function () {
-    const viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('preview'));
-    viewer.start();
-    viewer.setTheme('light-theme');
-    const urn = window.location.hash ? window.location.hash.substr(1) : null;
-    setupModelSelection(viewer, urn);
-    setupModelUpload(viewer);
+    const resp = await fetch('/api/models');
+    if (resp.ok) {
+        const models = await resp.json();
+        for (let i = 0; i < models.length && i < 4; i++) {
+            const viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById(`preview-${i + 1}`));
+            viewer.start();
+            viewer.setTheme('light-theme');
+            loadModel(viewer, models[i].urn);
+        }
+    } else {
+        alert('Could not list models. See the console for more details.');
+        console.error(await resp.text());
+    }
 });
 
 /**
@@ -26,77 +33,6 @@ async function getAccessToken(callback) {
 }
 
 /**
- * Initializes model selection UI. Can be called repeatedly to refresh the selection.
- * @async
- * @param {GuiViewer3D} viewer Forge Viewer instance.
- * @param {string} [selectedUrn] Optional model URN to mark as selected.
- */
-async function setupModelSelection(viewer, selectedUrn) {
-    const models = document.getElementById('models');
-    models.setAttribute('disabled', 'true');
-    models.innerHTML = '';
-    const resp = await fetch('/api/models');
-    if (resp.ok) {
-        for (const model of await resp.json()) {
-            const option = document.createElement('option');
-            option.innerText = model.name;
-            option.setAttribute('value', model.urn);
-            if (model.urn === selectedUrn) {
-                option.setAttribute('selected', 'true');
-            }
-            models.appendChild(option);
-        }
-    } else {
-        alert('Could not list models. See the console for more details.');
-        console.error(await resp.text());
-    }
-    models.removeAttribute('disabled');
-    models.onchange = () => loadModel(viewer, models.value);
-    if (!viewer.model && models.value) {
-        loadModel(viewer, models.value);
-    }
-}
-
-/**
- * Initializes model upload UI.
- * @async
- * @param {GuiViewer3D} viewer Forge Viewer instance.
- */
-async function setupModelUpload(viewer) {
-    const button = document.getElementById('upload');
-    const input = document.getElementById('input');
-    button.addEventListener('click', async function () {
-        input.click();
-    });
-    input.addEventListener('change', async function () {
-        if (input.files.length !== 1) {
-            return;
-        }
-        const file = input.files[0];
-        let data = new FormData();
-        data.append('model-name', file.name);
-        data.append('model-file', file);
-        // When uploading a zip file, ask for the main design file in the archive
-        if (file.name.endsWith('.zip')) {
-            const entrypoint = window.prompt('Please enter the filename of the main design inside the archive.');
-            data.append('model-zip-entrypoint', entrypoint);
-        }
-        button.setAttribute('disabled', 'true');
-        button.innerText = 'Uploading ...';
-        const resp = await fetch('/api/models', { method: 'POST', body: data });
-        if (resp.ok) {
-            input.value = '';
-            setupModelSelection(viewer);
-        } else {
-            alert('Could not upload model. See the console for more details.');
-            console.error(await resp.text());
-        }
-        button.innerText = 'Upload';
-        button.removeAttribute('disabled');
-    });
-}
-
-/**
  * Loads specific model into the viewer.
  * @param {Autodesk.Viewing.GuiViewer3D} viewer Instance of the viewer to load the model into.
  * @param {string} urn URN (base64-encoded object ID) of the model to be loaded.
@@ -109,6 +45,5 @@ function loadModel(viewer, urn) {
         alert('Could not load model. See the console for more details.');
         console.error(message);
     }
-    window.location.hash = urn;
     Autodesk.Viewing.Document.load('urn:' + urn, onDocumentLoadSuccess, onDocumentLoadFailure);
 }
